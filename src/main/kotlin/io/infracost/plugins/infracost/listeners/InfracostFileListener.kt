c26a5e143e23db29cb71d46cc86271dc329ee02e
Owen Rumney <owen@infracost.io>
2024-07-02 10:48:57 +0100

    fix: Don't run on each file save
    Only run infracost when the file that is update is a terraform file

--- src/main/kotlin/io/infracost/plugins/infracost/listeners/InfracostFileListener.kt
+++ src/main/kotlin/io/infracost/plugins/infracost/listeners/InfracostFileListener.kt
@@ -5,6 +5,9 @@ import com.intellij.openapi.vfs.newvfs.BulkFileListener
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent
 import io.infracost.plugins.infracost.actions.RunInfracostAction
 
+val INFRACOST_FILE_EXTENSIONS = setOf("tf", "hcl", "tfvars")
+val INFRACOST_FILES = setOf("infracost.yml", "infracost.yml.tmpl", "infracost-usage.yml")
+
 class InfracostFileListener : BulkFileListener {
 
     // This method is called after the files are processed
@@ -13,8 +16,11 @@ class InfracostFileListener : BulkFileListener {
     override fun after(events: MutableList<out VFileEvent>) {
         for (event in events) {
             if (event.isFromSave) {
-                val project = ProjectLocator.getInstance().guessProjectForFile(event.file!!) ?: return
-                RunInfracostAction.runInfracost(project)
+                if (event.file?.extension?.lowercase() in INFRACOST_FILE_EXTENSIONS ||
+                        INFRACOST_FILES.contains(event.file?.name?.lowercase())){
+                    val project = ProjectLocator.getInstance().guessProjectForFile(event.file!!) ?: return
+                    RunInfracostAction.runInfracost(project)
+                }
             }
         }
     }
